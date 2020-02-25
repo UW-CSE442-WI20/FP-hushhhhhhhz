8ae9a132574752153fd72b02fd1ea2fff77e548e
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2020-02-25 15:39:20 -0800

    add decrypt button and key input

--- src/symmetric.js
+++ src/symmetric.js
@@ -1,24 +1,49 @@
 const d3 = require('d3')
-const crypto = require('./crypto')
 
-var body = d3.select('#vis')
+var vis = d3.select('#vis')
 
-body.append('form')
+vis.append('form')
     .attr('id', 'form1')
     .attr('name', 'myform')
     .on('submit', handleClick)
     
 d3.select("#form1").append('input')
     .attr('type','submit')
-    .attr('name','Submit')
-    .attr('value','Encrypt')
+    .attr('value', 'Encrypt')
 
 d3.select("#form1").append('input')
     .attr('type','text')
-    .attr('id', 'input')
-    .attr('placeholder','text to encrypt')
+    .attr('id', 'message')
+    .attr('placeholder','Message to be sent')
 
+d3.select("#form1").append('input')
+    .attr('type','text')
+    .attr('id', 'secret')
+    .attr('placeholder','Key to be used (random when empty)')
 
-function handleClick(event){
-    console.log(document.getElementById("input").value)
+const Crypto = require('./crypto')
+var cryptoInstance;
+var encryption;
+
+function handleClick(){
+    d3.event.preventDefault();
+    let message = document.getElementById("message").value
+    let secret = document.getElementById("secret").value
+    console.log("key ", secret, " hey")
+    cryptoInstance = new Crypto(secret);
+    encryption = cryptoInstance.encrypt(message)
+    vis.append('text').text(encryption.ciphertext)
+    console.log(encryption.ciphertext)
 }
+
+function handleDecrypt() {
+    let decryption = cryptoInstance.decrypt(encryption.encrypted_data)
+    console.log(decryption)
+    vis.append('br');
+    vis.append('text').text(decryption)
+}
+
+vis.append('input')
+    .attr('type','submit')
+    .attr('value', 'Decrypt')
+    .on('click', handleDecrypt)

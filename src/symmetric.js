1cc2be772c4cc05d8dd052067434f32bef223301
Amir Mola <amirmola@cs.washington.edu>
2020-02-26 18:51:07 -0800

    working feature

--- src/symmetric.js
+++ src/symmetric.js
@@ -18,16 +18,19 @@ class Symmetric {
         this.encryption = this.cryptoInstance.encrypt(message)
         d3.select('#encryption_display').transition().text(this.encryption.ciphertext)
         console.log(this.encryption.ciphertext)
+        d3.select("#lock2").attr('src', lock)
         d3.select('#decrypt_button').attr('hidden', null)
         d3.select("#lock").attr('hidden', null)
+        d3.select("#lock2").attr('hidden', null)
+        d3.select("#key2").attr('hidden', null)
+
     }
     
     handleDecrypt() {
         let decryption = this.cryptoInstance.decrypt(this.encryption.encrypted_data)
         console.log(decryption)
         d3.select('#decryption_display').transition().text(decryption)
-        d3.select("#unlock2").attr('hidden', null)
-        d3.select("#key2").attr('hidden', null)
+        d3.select("#lock2").attr('src', unlock)
 
     }
 
@@ -48,62 +51,79 @@ class Symmetric {
             .attr('id', 'message')
             .attr('placeholder','Message to be sent')
         
-        d3.select("#form1").append("div")
-            .attr('id', 'secret_box')
+        // d3.select("#form1").append("div")
+        //     .attr('id', 'secret_box')
 
         // key
-        d3.select("#secret_box").append('input')
+        d3.select("#input_box").append('input')
             .attr('type','text')
             .attr('id', 'secret')
             .attr('placeholder','Key to be used (random when empty)')
         
-        d3.select("#input_box").append('img')
-            .attr('id', 'arrow')
-            .attr("src", arrow)
-            .attr('width', '10%')
-            .on('click', this.handleEncrypt)
-
-        d3.select("#input_box").append('text')
-            .attr('id', 'encryption_display')
-
-        d3.select("#input_box").append('img')
-            .attr('id', 'decrypt_button')
-            .attr("src", arrow)
-            .attr('width', '10%')
-            .attr('hidden', 'true')
-            .on('click', this.handleDecrypt)
-
-        d3.select("#input_box").append('text')
-            .attr('id', 'decryption_display')
+        // d3.select("#input_box").append('img')
+        //     .attr('id', 'arrow')
+        //     .attr("src", arrow)
+        //     .attr('width', '10%')
+        //     .on('click', this.handleEncrypt)
         
-        this.vis.append('img')
+        d3.select("#form1").append("div")
+            .attr('id', 'output_box')
+
+        d3.select("#output_box").append('text')
+            .attr('id', 'encryption_display')
+            .attr("style","margin-left: 50%")
+
+
+        // d3.select("#input_box").append('img')
+        //     .attr('id', 'decrypt_button')
+        //     .attr("src", arrow)
+        //     .attr('width', '10%')
+        //     .attr('hidden', 'true')
+        //     .on('click', this.handleDecrypt)
+
+        d3.select("#output_box").append('text')
+            .attr('id', 'decryption_display')
+            .attr("style","margin-left: 40%")
+        
+        this.vis.append("div")
+            .attr('id', 'shapes')
+
+        d3.select("#shapes").append('img')
             .attr('id', 'unlock')
             .attr("src", unlock)
             .attr('width', '10%')
 
-        this.vis.append('img')
+        d3.select("#shapes").append('img')
             .attr('id', 'key')
             .attr("src", key)
             .attr('width', '10%')
+            .on('click', this.handleEncrypt)
 
-        this.vis.append('img')
+        d3.select("#shapes").append('img')
             .attr('id', 'lock')
             .attr("src", lock)
             .attr('width', '10%')
             .attr("style","margin-left: 20%")
             .attr('hidden', 'true')
         
-        this.vis.append('img')
-            .attr('id', 'unlock2')
-            .attr("src", unlock)
+        d3.select("#shapes").append('img')
+            .attr('id', 'lock2')
+            .attr("src", lock)
             .attr('width', '10%')
             .attr("style","margin-left: 20%")
             .attr('hidden', 'true')
+        
+        // d3.select("#shapes").append('img')
+        //     .attr('id', 'unlock2')
+        //     .attr("src", unlock)
+        //     .attr('width', '10%')
+        //     .attr('hidden', 'true')
 
-        this.vis.append('img')
+        d3.select("#shapes").append('img')
             .attr('id', 'key2')
             .attr("src", key)
             .attr('width', '10%')
+            .on('click', this.handleDecrypt)
             .attr('hidden', 'true')
     }
 }

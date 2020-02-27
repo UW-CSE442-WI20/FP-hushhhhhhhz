9693df4f2423142bf8cde0e33df748bbdc524533
Emma <emraible@uw.edu>
2020-02-26 16:32:14 -0800

    barely started

--- src/create-history.js
+++ src/create-history.js
@@ -1,4 +1,4 @@
-
+const d3 = require('d3');
   
 // You can separate your code out into modules to
 // keep code clean.
@@ -11,10 +11,17 @@ class CreateHistory {
   start() {
     console.log('[CreateHistory]', 'Hello World 5.');
 	document.getElementById('vis').innerHTML = "";		
-  }
-  cont() {
-    console.log('[CreateHistory]', 'Hello World 5.');
-    document.getElementById('vis').innerHTML = "";
+	
+	var timelineContainer = d3.select('#vis').append('div').attr('class', 'timelineContainer');
+	var leftDiv = timelineContainer.append('div').attr('class', 'leftDiv');
+	var lineDiv = timelineContainer.append('div').attr('class', 'lineDiv');
+	var rightDiv = timelineContainer.append('div').attr('class', 'rightDiv');
+	var line = lineDiv.append('div').attr('class', 'line');
+
+	var firstEvent = leftDiv.append('div').attr('class', 'eventBox');
+	firstEvent.append('text').text("Event 1")
+	leftDiv.append('br')
+  	leftDiv.append('div').attr('class', 'eventBox');
   }
 }
 

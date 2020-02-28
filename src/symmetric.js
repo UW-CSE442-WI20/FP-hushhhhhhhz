8b77a9708a3cce0eca4e69cc5dbc2badbcea8189
Amir Mola <amirmola@cs.washington.edu>
2020-02-27 19:11:34 -0800

    kushal pop off please

--- src/symmetric.js
+++ src/symmetric.js
@@ -4,11 +4,13 @@ const lock = require('./images/lock.png')
 const unlock = require('./images/unlock.png')
 const unlocked_key = require('./images/unlocked_key.png')
 const locked_key = require('./images/locked_key.png')
+// const bootstrap = require('bootstrap')
 class Symmetric {
     constructor() {
         this.handleEncrypt = this.handleEncrypt.bind(this)
         this.handleDecrypt = this.handleDecrypt.bind(this)
         this.handleNewMessage = this.handleNewMessage.bind(this)
+        this.delayInMilliseconds = 1000; //1 second
     }
 
     handleEncrypt(){
@@ -24,7 +26,10 @@ class Symmetric {
         d3.select("#lock").attr('hidden', null)
         d3.select("#decrypt_button").attr('hidden', null)
         d3.select("#encryption_display").attr('hidden', null)    
-        d3.select("#decryption_display").attr('hidden', null)    
+        d3.select("#decryption_display").attr('hidden', null)   
+        d3.select("#text2").attr('hidden', null) 
+        d3.select("#text4").attr('hidden', "true")
+        d3.select("#text3").attr('hidden', null)      
 
     }
     
@@ -33,6 +38,10 @@ class Symmetric {
         console.log(decryption)
         d3.select('#decryption_display').attr('hidden', null).transition().text(decryption)
         d3.select("#decrypt_button").attr('src', unlock)
+        d3.select("#text3").attr('hidden', "true")
+        d3.select("#text4").attr('hidden', null)
+
+
     }
 
     handleNewMessage() {
@@ -41,6 +50,9 @@ class Symmetric {
         d3.select("#decrypt_button").attr('hidden', 'true')
         d3.select("#encryption_display").attr('hidden', 'true')
         d3.select("#decryption_display").attr('hidden', 'true')
+        d3.select("#text2").attr('hidden', "true") 
+        d3.select("#text4").attr('hidden', "true")
+
     }
 
     stopRKey(evt) {
@@ -52,14 +64,24 @@ class Symmetric {
     start() {
         document.getElementById('vis').innerHTML = "";
         this.vis = d3.select('#vis')
-
+        
         this.vis.append('div')
             .attr('id', 'randomdiv')
+            // .attr('class', 'container')
+            .attr("style", "margin-top:20%")
 
         d3.select('#randomdiv').append("div")
             .attr('id', 'first')
+            // .attr('class', 'row')
 
-        d3.select('#first').append("div")
+        // d3.select('#first_row')
+        //     .append("div")
+        //     .attr('id', 'first_col')
+
+        // d3.select('#first_col').append("div")
+        //     .attr('id', 'input_boxes')
+
+         d3.select('#first').append("div")
             .attr('id', 'input_boxes')
 
         d3.select('#input_boxes').append('form')
@@ -88,6 +110,13 @@ class Symmetric {
             .attr('id', 'unlocked_key')
             .attr("src", unlocked_key)
             .on('click', this.handleEncrypt)
+        
+        d3.select('#first').append("div")
+            .attr('id', 'text1')
+            .style("border", "1px black solid")
+            .style("margin-top", "10%")
+            .style("margin-right", "15%")
+            .text("Try typing in a message and click on the lock to encrypt it")
 
         d3.select('#randomdiv').append("div")
             .attr('id', 'second')
@@ -96,7 +125,8 @@ class Symmetric {
             .attr('id', 'encrypted')
 
         d3.select("#encrypted").append('textarea')
-            .attr('style', "resize: none;")
+            .attr('style', "resize: none;background-color: white;")
+            .attr('disabled', 'true')
             .attr('id', 'encryption_display')
             .attr('hidden', 'true')
 
@@ -108,6 +138,12 @@ class Symmetric {
             .attr("src", lock)
             .attr('width', '140')
             .attr('hidden', 'true')
+        
+        d3.select('#second').append("div")
+            .attr('id', 'text2')
+            .style("border", "1px black solid")
+            .text("This is the encrypted message, visible to everyone")
+            .attr('hidden', 'true')
 
         d3.select('#randomdiv').append("div")
             .attr('id', 'third')
@@ -116,7 +152,8 @@ class Symmetric {
             .attr('id', 'decrypted')
 
         d3.select("#decrypted").append('textarea')
-            .attr('style', "resize: none;")
+            .attr('style', "resize: none;background-color: white;")
+            .attr('disabled', 'true')
             .attr('id', 'decryption_display')
             .attr('hidden', 'true')
         
@@ -129,6 +166,19 @@ class Symmetric {
             .attr('width', '160')
             .attr('hidden', 'true')
             .on('click', this.handleDecrypt)
+
+        d3.select('#third').append("div")
+            .attr('id', 'text3')
+            .style("border", "1px black solid")
+            .text("Only with the key that we used to encrypt the message, we can decrypt it! Click on the lock to decrypt")
+            .attr('hidden', 'true')
+        
+        d3.select('#third').append("div")
+            .attr('id', 'text4')
+            .style("border", "1px black solid")
+            .text("The decrypted message is only visible to the keyholders")
+            .attr('hidden', 'true')
+
 
         document.onkeypress = this.stopRKey; 
     }

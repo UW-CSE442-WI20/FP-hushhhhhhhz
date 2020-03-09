23b94bd5c134d593eb9ea0c4ec531da7472f60b6
Kushal Jhunjhunwalla <kushal.jhunjhunwalla@gmail.com>
2020-03-08 20:12:35 -0700

    skeleton with top down transition

--- src/rsa.js
+++ src/rsa.js
@@ -9,19 +9,122 @@ class RSA {
     start() {
         document.getElementById('vis').innerHTML = "";
 
-        // Sender Step 1
+        let overall_container = d3.select('#vis').append('div').attr('class', 'overall_Container');
 
-        initialMessage = "SECRET";
-        initialCipher = Array.from(initialMessage).map((x) => x.charCodeAt(0));
+        overall_container.append('div').attr('class', 'receiver_intro').text('Receiver\'s workflow');
+        overall_container.append('div').text('Select 2 primes')
 
-        let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
-        let alphabet2 = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-        let alphaAlpha = alphabet.concat(alphabet2)
+        // RECEIVER
 
-        let rsa_container = d3.select('#vis').append('div').attr('class', 'rsaContainer');
-        let message = rsa_container.append('div').attr('class', 'message');
+        let calc = overall_container.append('div').attr('class', 'calculation')
 
-        for (let i = 0; i < initialMessage.length; i++) {
+        // Block for transition left to right (if needed)
+        // let p_calculation = ['p', '=', '11']
+        // let q_calculation = ['q', '=', '17']
+        // for (let i = 0; i < p_calculation.length; i++) {
+        //     let index = calc.append('div')
+        //         .attr('id', 'p_index' + i)
+        //         .attr('class', 'p_index')
+        //         .style('display', 'block');
+
+        //     index.append('div')
+        //         .attr('class', 'p_data')
+        //         .transition()
+        //         // .duration(20000)
+        //         .text(p_calculation[i]);
+
+        //     index.append('div')
+        //         .attr('class', 'q_data')
+        //         .transition()
+        //         // .duration(10000)
+        //         .text(q_calculation[i]);
+        // }
+
+        calc.append('div')
+            .attr('id', 'p_data')
+            .transition()
+            .duration(2000)
+            .on('start', () => {
+                d3.select('#p_data').text('p = 11')
+            })
+            .on('end', () => {
+                d3.select('.calculation')
+                    .transition()
+                    .duration(2000)
+                    .on('start', () => {
+                        d3.select('.calculation')
+                            .append('div')
+                            .attr('id', 'q_data').text('q = 17')
+                    })
+                    .on('end', () => {
+                        d3.select('.calculation')
+                            .transition()
+                            .duration(2000)
+                            .on('start', () => {
+                                d3.select('.calculation')
+                                    .append('div')
+                                    .attr('id', 'n_data').text('n = pq = 11 * 17 = 187')
+                            })
+                            .on('end', () => {
+                                d3.select('.calculation')
+                                    .transition()
+                                    .duration(2000)
+                                    .on('start', () => {
+                                        d3.select('.calculation')
+                                            .append('div')
+                                            .attr('id', 'phi_data').text('ϕ(n) = (p-1)(q-1) = 10 * 16 = 160')
+                                    })
+                                    .on('end', () => {
+                                        d3.select('.calculation')
+                                            .transition()
+                                            .duration(2000)
+                                            .on('start', () => {
+                                                d3.select('.calculation')
+                                                    .append('div')
+                                                    .attr('id', 'e_data').text('e = 3 such that e is relatively prime to ϕ(n)')
+                                            })
+                                            .on('end', () => {
+                                                d3.select('.calculation')
+                                                    .transition()
+                                                    .duration(2000)
+                                                    .on('start', () => {
+                                                        d3.select('.calculation')
+                                                            .append('div')
+                                                            .attr('id', 'd_data').text('d = 107 such that d * e ≡ 1 (mod ϕ(n))')
+                                                    })
+                                                    .on('end', () => {
+                                                        d3.select('.calculation')
+                                                            .transition()
+                                                            .duration(2000)
+                                                            .on('start', () => {
+                                                                d3.select('.calculation')
+                                                                    .append('div')
+                                                                    .attr('id', 'public_data').text('n and e are made public and d is the secret privately held by the receiver')
+                                                            })
+                                                            .on('end', this.senderMessage)
+                                                    })
+                                            })
+                                    })
+                            })
+                    })
+            })
+    }
+
+    senderMessage() {
+                // Sender Step 1
+                initialMessage = "SECRET";
+                initialCipher = Array.from(initialMessage).map((x) => x.charCodeAt(0));
+
+                let alphabet =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
+                let alphabet2 =["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+                let alphaAlpha = alphabet.concat(alphabet2)
+
+        d3.select('#vis').append('div').text('Sender\'s workflow');
+
+                let rsa_container = d3.select('#vis').append('div').attr('class', 'rsaContainer');
+                let message = rsa_container.append('div').attr('class', 'message');
+
+                for(let i = 0; i<initialMessage.length; i++) {
             let index = message.append('div')
                 .attr('id', 'index' + i)
                 .attr('class', 'index');

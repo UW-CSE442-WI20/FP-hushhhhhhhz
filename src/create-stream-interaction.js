632d0957edf76cfea00141f7ded0a2e7f825a3d4
hmharvey <hmharvey@github.com>
2020-03-11 12:09:08 -0700

    minor indentation

--- src/create-stream-interaction.js
+++ src/create-stream-interaction.js
@@ -15,7 +15,7 @@ class StreamInteraction {
 		var cipherColor = "var(--cipher-color)";
 
 		d3.selectAll(".fullVis:not(.special)").html("")
-        d3.selectAll('.halfVis').html("")
+		d3.selectAll('.halfVis').html("")
 
 		inputMessage = "";
 		inputKey = "";
@@ -46,34 +46,34 @@ class StreamInteraction {
 			.text("plaintext: ")
 			.style('color', plainColor)
 		var plainChoice = plainDiv.append('div')
-            .append('h2')
-            .text("")
+			.append('h2')
+			.text("")
 			.attr('id', 'plainChoice')
-            .style('color', plainColor)
+			.style('color', plainColor)
 
 		var keyDiv = results.append('div').attr('class', 'resultsDivs')
-        var keyDisplay = keyDiv.append('div')
-            .attr('id', 'keyDisplay')
-            .append('h2')
-            .text("keytext: ")
-            .style('color', keyColor)
-        var keyChoice = keyDiv.append('div')
-            .append('h2')
-            .text("")
-            .attr('id', 'keyChoice')
-            .style('color', keyColor)
+		var keyDisplay = keyDiv.append('div')
+			.attr('id', 'keyDisplay')
+			.append('h2')
+			.text("keytext: ")
+			.style('color', keyColor)
+		var keyChoice = keyDiv.append('div')
+			.append('h2')
+			.text("")
+			.attr('id', 'keyChoice')
+			.style('color', keyColor)
 
 		var cipherDiv = results.append('div').attr('class', 'resultsDivs')
-        var cipherDisplay = cipherDiv.append('div')
-            .attr('id', 'cipherDisplay')
-            .append('h2')
-            .text("ciphertext: ")
-            .style('color', cipherColor)
-        var cipherChoice = cipherDiv.append('div')
-            .append('h2')
-            .text("")
-            .attr('id', 'cipherChoice')
-            .style('color', cipherColor)
+		var cipherDisplay = cipherDiv.append('div')
+			.attr('id', 'cipherDisplay')
+			.append('h2')
+			.text("ciphertext: ")
+			.style('color', cipherColor)
+		var cipherChoice = cipherDiv.append('div')
+			.append('h2')
+			.text("")
+			.attr('id', 'cipherChoice')
+			.style('color', cipherColor)
 
 		var row1 = ["RNC","ONQ","CKO","KEY","FGU"];
 		var row2 = ["MIO","CAY","ZUU","KRK","BMW"];
@@ -119,7 +119,7 @@ class StreamInteraction {
 		}
 		function handleMouseOver() {  // Add interactivity
 			d3.select(this).style('background-color', keyColor)
-			               .style('color', 'black')
+				.style('color', 'black')
 			inputKey = this.id
 			var newKey = "" 
 			if (inputMessage.length > 0) {
@@ -134,7 +134,7 @@ class StreamInteraction {
 		}
 		function handleMouseOut() {
 			d3.select(this).style('background-color', 'transparent')      
-			               .style('color', 'white')
+				.style('color', 'white')
 			inputKey = ""
 			resultCipher = ""
 			d3.select("#" + savedId).style('color', keyColor)
@@ -158,7 +158,7 @@ class StreamInteraction {
 			.attr('class', 'keyChoice')
 			.on("mouseover", handleMouseOver)
 			.on("mouseout", handleMouseOut)
-		    .on("click", handleClick);
+			.on("click", handleClick);
 		d3.select('#inputButton').on('click', function () { 
 			inputMessage = document.getElementById('textInput').value 
 			inputMessage = inputMessage.replace(/\s+/g, '');
@@ -168,85 +168,104 @@ class StreamInteraction {
 		});
 
 		var title3 = d3.select('#title4 .fullVis').append('div').attr('class', 'title3Container');
-        title3.append('h2').text('Decryption:')
+		title3.append('h2').text('Decryption:')
 		var input2Container = d3.select('#title4 .fullVis').append('div').attr('class', 'input2Container');
 		var inputs2 = input2Container.append('div').attr('class', 'inputs2'); 
 		inputs2.append('h4').text('try to decode this secret message by reversing the equation (C - K mod 26 = P)').attr('class', 'instruction')
-		inputs2.append('h2').text('ciphertext: ISSNMBSX').style('color', cipherColor)
-		inputs2.append('h2').text('keytext: KEYKEYKE').style('color', keyColor)
+		var decryptDiv = inputs2.append('div').attr('class', 'resultsDivs')
+		decryptDiv.append('div')
+			.attr('id', 'cipherDisplay')
+			.append('h2')
+			.text('ciphertext: ')
+			.style('color', cipherColor)
+		decryptDiv.append('div')
+			.append('h2')
+			.text("ISSNMBSX")
+			.style('color', cipherColor)
+
+		var decryptDiv2 = inputs2.append('div').attr('class', 'resultsDivs')
+		decryptDiv2.append('div')
+			.attr('id', 'keyDisplay')
+			.append('h2')
+			.text('keytext: ')
+			.style('color', keyColor)
+		decryptDiv2.append('div')
+			.append('h2')
+			.text("KEYKEYKE")
+			.style('color', keyColor)
 
 		var table_div2 = input2Container.append('div').attr('class', 'tableDiv')
 		lilDiv2 = input2Container.append('div').attr('class', 'lilDiv2')
-        table_div2.append('h4').text('use this table to compute the values of C and K:').style('margin', '0 0 30px 0')
+		table_div2.append('h4').text('use this table to compute the values of C and K:').style('margin', '0 0 30px 0')
 		var table = table_div2.append('table');
-        var thead = table.append('thead');
-        var tbody = table.append('tbody');
-        thead.append('tr')
-            .selectAll('th')
-            .data(alphabet)
-            .enter()
-            .append('th')
-            .text(function(letter) { return letter })
-            .attr('class', function(letter) { return letter });
-        var rows = tbody.selectAll('tr')
-            .data([1])
-            .enter()
-            .append('tr')
-        var cells = rows.selectAll('td')
-            .data(function(row) {
-                return alphabet.map(function(letter) {
-                    return {column: letter, value: alphabet.indexOf(letter)};
-                });
-            })
-            .enter()
-            .append('td')
-            .html(function(d) {return d.value})
-            .attr('class', function(d) { return d.column });
+		var thead = table.append('thead');
+		var tbody = table.append('tbody');
+		thead.append('tr')
+			.selectAll('th')
+			.data(alphabet)
+			.enter()
+			.append('th')
+			.text(function(letter) { return letter })
+			.attr('class', function(letter) { return letter });
+		var rows = tbody.selectAll('tr')
+			.data([1])
+			.enter()
+			.append('tr')
+		var cells = rows.selectAll('td')
+			.data(function(row) {
+				return alphabet.map(function(letter) {
+					return {column: letter, value: alphabet.indexOf(letter)};
+				});
+			})
+			.enter()
+			.append('td')
+			.html(function(d) {return d.value})
+			.attr('class', function(d) { return d.column });
 		var table2 = table_div2.append('table');
-        var thead2 = table2.append('thead');
-        var tbody2 = table2.append('tbody');
-        thead2.append('tr')
-            .selectAll('th')
-            .data(alphabet2)
-            .enter()
-            .append('th')
-            .text(function(letter) { return letter })
-            .attr('class', function(letter) { return letter });
-        var rows2 = tbody2.selectAll('tr')
-            .data([1])
-            .enter()
-            .append('tr')
-        var cells2 = rows2.selectAll('td')
-            .data(function(row) {
-                return alphabet2.map(function(letter) {
-                    return {column: letter, value: alphabet2.indexOf(letter) + 13};
-                });
-            })
-            .enter()
-            .append('td')
-            .html(function(d) {return d.value})
-            .attr('class', function(d) { return d.column });
-		
+		var thead2 = table2.append('thead');
+		var tbody2 = table2.append('tbody');
+		thead2.append('tr')
+			.selectAll('th')
+			.data(alphabet2)
+			.enter()
+			.append('th')
+			.text(function(letter) { return letter })
+			.attr('class', function(letter) { return letter });
+		var rows2 = tbody2.selectAll('tr')
+			.data([1])
+			.enter()
+			.append('tr')
+		var cells2 = rows2.selectAll('td')
+			.data(function(row) {
+				return alphabet2.map(function(letter) {
+					return {column: letter, value: alphabet2.indexOf(letter) + 13};
+				});
+			})
+			.enter()
+			.append('td')
+			.html(function(d) {return d.value})
+			.attr('class', function(d) { return d.column });
+
 		lilDiv2.append('text').text('try your answer below:')
 		var textInput2 = lilDiv2.append('input')
-            .attr('id', 'textInput2')
-            .attr('style', 'text')
+			.attr('id', 'textInput2')
+			.attr('style', 'text')
 		var inputButton2 = lilDiv2.append('div')
-            .attr('id', 'inputButton2')
-            .text('CHECK')
+			.attr('id', 'inputButton2')
+			.text('CHECK')
 		var answer = lilDiv2.append('h4').text('')
-								.style('color', 'white')
-								.attr('id', 'answer')
+			.style('color', 'white')
+			.attr('id', 'answer')
 		d3.select('#inputButton2').on('click', function () {
-            guess = document.getElementById('textInput2').value
-            guess = guess.replace(/\s+/g, '');
+			guess = document.getElementById('textInput2').value
+			guess = guess.replace(/\s+/g, '');
 			guess = guess.toUpperCase();
 			if (guess == "YOUDIDIT") {
-            	document.getElementById('answer').innerHTML = "correct!";
-            } else {
+				document.getElementById('answer').innerHTML = "correct!";
+			} else {
 				document.getElementById('answer').innerHTML = "try again..."
 			}
-        });
+		});
 	}
 }
 

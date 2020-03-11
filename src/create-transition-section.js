2bdde8b1afc736a9a5d8429ee9951d92fdbea373
Emma <emraible@uw.edu>
2020-03-11 13:06:06 -0700

    added to transition

--- src/create-transition-section.js
+++ src/create-transition-section.js
@@ -10,13 +10,13 @@ class TransitionSection {
 
 		colors = ["#FFFFFF", "#BCF2F0", "#4EB7B2", "#2B7A78"];
 
-        futureContainer = d3.select("#title7 .fullVis")
+        transitionContainer = d3.select("#title7 .fullVis")
 
 		var nodes = [
             { id: "1", r: 1000, cx: 750, cy: 400, color: "#2B7A78"}
 		];
 
-		var futureContainer = futureContainer.append('svg')
+		var futureContainer = transitionContainer.append('svg')
                 .style('width','3000')
                 .style('height', '1000')
                 .append('g');
@@ -28,7 +28,7 @@ class TransitionSection {
                 .attr('fill',  function (d) { return d.color; })
                 .attr('cx', function (d) { return d.cx; })
                 .attr('cy', function (d) { return d.cy; })
-				.style('opacity', 0.5);
+				.style('opacity', 0.7);
 
 		nodeElements.transition("grow")
                 .duration(2000)
@@ -42,8 +42,73 @@ class TransitionSection {
                 .attr('opacity', 0)
                 .transition()
                 .duration(2000)
-                .attr('opacity', 100)
+                .attr('opacity', 1)
                 .delay(1000)
+
+		bubbleContainer = transitionContainer.append('div')
+			.style('width', '900px')
+			.style('position','fixed')
+			.style('top','20vh')
+			.style('left','25vw')
+			.style('text-align', 'center')
+
+		firstBubble = bubbleContainer.append('div')
+                .style("width", "0px")
+                .style("height", "0px")
+                .style("background-color", "#1f2833")
+                .attr("class", "explanationCircle")
+		
+            firstBubble.append("div")
+                .attr('class', 'textDiv')
+                .attr('class', 'textDivTitle')
+                .text("Into Data Protection")
+                .style("opacity", 0)
+
+        firstBubble.transition()
+                .duration(1000)
+                .style("width", "400px")
+                .style("height", "400px")
+				.delay(2000)
+
+            firstBubble.selectAll(".textDiv").transition()
+                .duration(500)
+                .style("opacity", 1)
+                .delay(2500)
+
+            firstBubble.selectAll(".textDivTitle").transition()
+                .duration(500)
+                .style("opacity", 1)
+                .delay(2500)
+
+		secondBubble = bubbleContainer.append('div')
+                .style("width", "0px")
+                .style("height", "0px")
+                .style("background-color", "#314155")
+                .attr("class", "explanationCircle")
+				.style("margin-left", "auto")
+            secondBubble.append("div")
+                .attr('class', 'textDiv')
+                .attr('class', 'textDivTitle')
+                .text("Advanced Key Encryption")
+                .style("opacity", 0)
+
+        secondBubble.transition()
+                .duration(1000)
+                .style("width", "400px")
+                .style("height", "400px")
+                .delay(2700)
+				.style("margin-top", '-180px')
+
+            secondBubble.selectAll(".textDiv").transition()
+                .duration(500)
+                .style("opacity", 1)
+                .delay(3200)
+
+            secondBubble.selectAll(".textDivTitle").transition()
+                .duration(500)
+                .style("opacity", 1)
+                .delay(3200)
+
 /*
         var nodes = [
             { id: "1", r: 5, cx: 162, cy: 350, color: 0},
